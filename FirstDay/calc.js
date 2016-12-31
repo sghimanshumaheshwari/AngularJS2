53985be68fabb199408eebb00c7db3031201403a
sghimanshumaheshwari <sghimanshumaheshwari@outlook.com>
2016-12-31 19:49:02 +0530

    chnages

--- FirstDay/calc.js
+++ FirstDay/calc.js
@@ -1,11 +1,14 @@
 var Calc = (function () {
     function Calc() {
+        console.log('this is the constructor called');
     }
     Calc.prototype.sum = function (num1, num2) {
-        console.log(num1 + num2);
+        var _sum;
+        _sum = num1 + num2;
+        console.log(_sum);
         return (num1 + num2);
     };
     return Calc;
 }());
 var c = new Calc();
-c.sum(10, 20);
+c.sum(10, 40);
--- FirstDay/calc.ts
+++ FirstDay/calc.ts
@@ -1,11 +1,19 @@
 class Calc{
+constructor(){
+
+console.log('this is the constructor called');
+
+}
+
 
 sum(num1:number, num2:number):number{
-    console.log(num1+num2);
+    let _sum : number;
+    _sum=num1+num2;
+    console.log(_sum);
 return (num1+num2);
 }
 }
 
 var c=new Calc();
-c.sum(10,20);
+c.sum(10,40);
 
